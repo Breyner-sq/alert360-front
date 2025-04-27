e0176ba3ef91024abecec4613745f5d8083efd87
Breyner-sq <breynersanchezquintero@gmail.com>
2025-04-26 23:19:49 -0500

    Cambio de iconos

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="w-full flex items-center justify-center min-h-screen h-screen">
       <div className="2xl:max-w-8xl md:flex-row flex-col p-4 w-full h-full flex items-center justify-center gap-8 min-h-screen">
         <Image
-          className="select-none max-md:basis-1/2 md:size-[20rem] p-8"
+          className="select-none max-md:basis-1/2 md:size-[20rem] lg:size-[26rem] xl:size-[30rem] p-8"
           src="/logo.svg"
           alt="Alert360 logo"
           width={360}
